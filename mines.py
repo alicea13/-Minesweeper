f6a23cf976e9fed3aaef0af50470b1fe8677035f
alisa <alica.aisina@yandex.ru>
2019-12-04 23:50:15 +0300

    Добавили новую версию как выводить числа

--- mines.py
+++ mines.py
@@ -14,21 +14,20 @@ class Board:
         self.cell_size = 0
         self.c_min = mines
         self.set_view(left, top, cell_size)
-        self.color = ["blue", "yellow", "green", "white", "pink", "gray"]
         self.list_mines = []
-        for _ in range(self.c_min):
+        while len(self.list_mines) != self.c_min:
             cell = (random.randint(0, self.height - 1),
                     random.randint(0, self.width - 1))
             if cell not in self.list_mines:
                 self.list_mines.append(cell)
         print('\n'.join(str(i) for i in self.list_mines))
         self.board = [[0] * width for _ in range(self.height)]
-        for j in range(self.width):
-            for i in range(self.height):
-                if (i, j) not in self.list_mines:
-                    self.board[i][j] = - 1
+        for y in range(self.width):
+            for x in range(self.height):
+                if (x, y) not in self.list_mines:
+                    self.board[x][y] = - 1
                 else:
-                    self.board[i][j] = 10
+                    self.board[x][y] = 10
         print('\n'.join(str(i) for i in self.board))
 
     # настройка внешнего вида
@@ -72,33 +71,59 @@ class Board:
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
         if cell:
-            board.open_cell()
+            if self.board[cell[0]][cell[1]] != 10:
+                board.open_cell(cell[0], cell[1])
+        return cell[0], cell[1]
 
 
 class Minesweeper(Board):
     def __init__(self, width, height, mines, left=10, top=10, cell_size=520):
         super().__init__(width, height, mines, left, top, cell_size)
 
-    def open_cell(self):
+    def render(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                pygame.draw.rect(screen, pygame.Color("white"),
+                                 ((self.left + self.cell_size * i, self.top + self.cell_size * j),
+                                 (self.cell_size, self.cell_size)), 1)
+                if self.board[j][i] == 10:
+                    pygame.draw.rect(screen2, pygame.Color("red"),
+                                     ((self.left + self.cell_size * i,
+                                       self.top + self.cell_size * j),
+                                      (self.cell_size, self.cell_size)))
+                elif self.board[j][i] == -1:
+                    pygame.draw.rect(screen2, pygame.Color("black"),
+                                     ((self.left + self.cell_size * i,
+                                       self.top + self.cell_size * j),
+                                      (self.cell_size, self.cell_size)))
+
+                # выводить число мин вокруг выбранной клетки
+                #elif self.board[j][i] != 10 and self.board[j][i] != -1:
+                    #screen.blit(self.bombs, (y0 * self.width, x0 * self.height))
+
+    def open_cell(self, x0, y0):
+        print(f"cell {x0, y0}")
         temp = copy.deepcopy(self.board)  # сохраняем поле для дальнейшего изменения текущего
-        for x in range(self.width):
-            l = 0
-            for y in range(self.height):
-                count2 = 0
-                count = 0
-                for dx in range(-1, 2):
-                    for dy in range(-1, 2):
-                        if (x + dx < 0 or y + dy < 0
-                                or x + dx >= self.width or y + dy >= self.height):
-                            continue
-                        else:
-                            count += self.board[y + dy][x + dx]
-            count -= self.board[y][x]
-            l += count
-            temp[y][x] = count2
+        self.count = 0
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                if (x0 + dx < 0 or y0 + dy < 0
+                        or y0 + dy >= self.width or x0 + dx >= self.height):
+                    continue
+                else:
+                    print(x0 + dx, y0 + dy)
+                    if self.board[x0 + dx][y0 + dy] == 10:
+                        self.count += 1
+                        print(f"bomb {x0 + dx, y0 + dy}")
+        temp[x0][y0] = self.count
         self.board = copy.deepcopy(temp)
-        print(l)
         print('\n'.join(str(i) for i in self.board))
+
+        # создаем объект-шрифт, который будем рендерить и накладывать на screen
+        fon = pygame.font.Font(None, 100)
+        self.bombs = fon.render(str(self.count), 1,
+                           pygame.Color("green"))
+        # screen.blit(self.bombs, (y0 * self.width, x0 * self.height))
 
 
 pygame.init()
